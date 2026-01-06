f0edf116e364e621098a5ba047379142817ab0ee
NitinGTAstrerix <nitinsonawane@gtasterix.com>
2026-01-06 11:11:55 +0530

    folder structure

---
+++ quvouch/src/main/java/com/app/quvouch/QuvouchApplication.java
@@ -0,0 +1,13 @@
+package com.app.quvouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuvouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuvouchApplication.class, args);
+	}
+
+}
---
+++ quvouch/src/test/java/com/app/quvouch/QuvouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.quvouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuvouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
