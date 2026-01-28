3065faeae94d5b1a7516bf2239e7f121282f8e80
Yash Harde <yashharde@gtasterix.com>
2026-01-28 13:13:31 +0530

    Revert "Review API Commit"
    This reverts commit 5bf11a028df17a55a46dac072ee7772aadf1e8fa.

--- src/main/java/com/spring/jwt/service/ReviewService.java
+++
@@ -1,8 +0,0 @@
-package com.spring.jwt.service;
-
-import com.app.quvouch.entity.Review;
-import com.app.quvouch.dtos.ReviewRequestDto;
-
-public interface ReviewService {
-    Review submitReview(ReviewRequestDto requestDto);
-}
