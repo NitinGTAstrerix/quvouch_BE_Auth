f5d4f6b14578e64d23171deae0ff70386dda4bb8
NitinGTAstrerix <nitinsonawane@gtasterix.com>
2026-02-12 12:54:31 +0530

    add the sale representative info by id

--- src/main/java/com/spring/jwt/controller/AdminController.java
+++ src/main/java/com/spring/jwt/controller/AdminController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,15 @@ public class AdminController {
     @GetMapping("/sale-representatives")
     public ResponseEntity<List<SaleRepresentativeInfo>> getSaleReps() {
         return ResponseEntity.ok(adminService.getAllSaleRepsWithClientCount());
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/sale-representatives/{id}")
+    public ResponseEntity<UserDTO> getSaleRepById(@PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                adminService.getSaleRepresentativeById(id)
+        );
     }
 }
 
